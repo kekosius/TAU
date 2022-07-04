701364361f29c19f05f057efc03da345022ef4a3
kekosius <57643273+kekosius@users.noreply.github.com>
2022-07-04 15:54:32 +0300

    Add files via upload
    Modeling of linear dynamical systems

---
+++ 1/script.m
@@ -0,0 +1,14 @@
+x = ScopeData(:,1);
+y = ScopeData(:,3);
+x1 = ScopeData2(:,1);
+y1 = ScopeData2(:,2); 
+p = plot(x,y,x1,y1,'--');
+%p = plot(x,y,'r');
+p(1).LineWidth = 2;
+p(2).LineWidth = 2;
+title('Многоканальная линейная динамическая ВСВ, u(t) = 2sin(t)');
+xlabel('Время, с');
+ylabel('Сигнал');
+grid on;
+legend({'y2(t)', 'u2(t)'}, 'FontSize',12);
+%legend({'y(t)'}, 'FontSize',12);
